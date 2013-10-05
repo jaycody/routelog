ec55aa56beaab9c9e6ac009f5a754eac4c797679
Matthew Story <matt.story@axialmarket.com>
2013-10-05 11:57:36 -0400

    fix path for routelog.5
    Signed-off-by: Matthew Story <matt.story@axialmarket.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='matt.story@axial.net',
     scripts=['bin/routelog'],
     data_files=[('share/man/man1', [ 'man/man1/routelog.1', ], ),
-                ('share/man/man5', [ 'man/man7/routelog.5', ], ), ],
+                ('share/man/man5', [ 'man/man5/routelog.5', ], ), ],
     url='https://github.com/axialmarket/routelog',
     license='3-BSD',
     description='A Flexible DSL For Processing Logs',
